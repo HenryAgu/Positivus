d92ef1517b9f89bf9f6d7c7e5176f17db005bcb2
Henry Agu <aguhenrychuks@gmail.com>
2025-03-11 09:11:34 +0100

    feat: mobile hero

--- src/App.vue
+++ src/App.vue
@@ -15,34 +15,38 @@ export default {
     <Navbar />
     <main class="min-h-screen">
       <!-- Hero -->
-
-      <section class="my-10 flex justify-between">
-        <div class="basis-[50%] flex flex-col gap-y-[35px]">
-          <h1 class="text-black font-medium text-[60px] leading-[70px]">
+      <section class="my-5 xl:my-10 flex flex-col-reverse gap-y-5 xl:flex-row justify-between px-4 xl:px-0">
+        <div class="basis-full xl:basis-[50%] flex flex-col gap-y-[35px]">
+          <h1 class="text-black font-medium text-[60px] leading-[70px] hidden xl:block">
             Navigating the <br />
             digital landscape <br />
             for success
           </h1>
           <p
-            class="text-xl font-normal text-black max-w-[498px] leading-[28px]"
+            class="text-base xl:text-xl font-normal text-black max-w-[498px] leading-[24px] xl:leading-[28px]"
           >
             Our digital marketing agency helps businesses grow and succeed
             online through a range of services including SEO, PPC, social media
             marketing, and content creation.
           </p>
           <button
-            class="w-fit bg-[#191A23] rounded-[14px] text-white font-normal text-xl py-5 px-[35px]"
+            class="w-full xl:w-fit bg-[#191A23] rounded-[14px] text-white font-normal text-xl py-5 px-[35px]"
           >
             Book a consultation
           </button>
         </div>
-        <div class="basis-[50%] flex justify-end">
+        <div class="basis-full xl:basis-[50%] flex justify-end">
           <img
             src="/images/Hero.png"
             alt="hero-image"
-            class="w-[600.4620971679688px] h-[515px]"
+            class="w-[361.44px] h-[310px] xl:w-[600.46px] xl:h-[515px]"
           />
         </div>
+        <h1 class="text-black font-medium text-[43px] leading-[55px] block xl:hidden">
+            Navigating the <br />
+            digital landscape <br />
+            for success
+          </h1>
       </section>
     </main>
     <Footer />
