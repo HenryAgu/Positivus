1175b003ebbb009b88422eec1796aa51a99dc7ea
Henry Agu <aguhenrychuks@gmail.com>
2025-03-13 11:43:07 +0100

    fix: team columns

--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -40,7 +40,7 @@ export default {
 </script>
 
 <template>
-  <section class="my-20 flex flex-col gap-y-5">
+  <section class="my-14 flex flex-col gap-y-5">
     <div class="" v-for="(faq, index) in faqs" :key="index">
       <div
         class="border-[#191A23] border bg-[#F3F3F3] p-[30px] lg:p-10 rounded-[45px] shadow-[0px_5px_0px_0px_#191A23]"
